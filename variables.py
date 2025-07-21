fa5be52ed7545454a91231d17aecaa508fb3bee9
Annie <vnquynhv2007@gmail.com>
2025-07-21 22:34:39 +0700

    Add variables.py and rename main.py to print.py

---
+++ variables.py
@@ -0,0 +1,19 @@
+# Strings
+first_name = "Annie"
+food = "ice cream" 
+email = "randomemail@gmail.com"
+
+# Integers and Floats
+age = 18
+quantity = 3
+price = 10.99
+
+# Booleans
+is_student = True
+
+print("Your name is", first_name)
+
+if is_student:
+    print("You are a student")
+else:
+    print("You are NOT a student")
