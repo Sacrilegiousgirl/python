52f785ca357e89a586e0d6922dc4b83ce8c0a45b
Annie <vnquynhv2007@gmail.com>
2025-07-26 22:42:10 +0700

    Add EX 15 and lesson 20

---
+++ lessons/20) functions.py
@@ -0,0 +1,45 @@
+
+def happy_birthday(name, age):
+    print(f"Happy birthday to {name}!")
+    print(f"You are {age} years old!")
+    print("Happy birthday to you!")
+    print()
+
+happy_birthday("Bro", 20)
+happy_birthday("Steve", 30)
+happy_birthday("Joe", 40)
+
+def display_invoice(username, amount, due_date):
+    print(f"Hello {username}")
+    print(f"Your bill of ${amount:.2f} is due: {due_date}")
+
+display_invoice("Annie", 42.50, "01/01")
+
+def add(x, y):
+    z = x + y
+    return z
+
+def subtract(x, y):
+    z = x - y
+    return z
+
+def multiply(x, y):
+    z = x * y
+    return z
+
+def divide(x, y):
+    z = x / y
+    return z
+
+print(add(1,2))
+print(subtract(1,2))
+print(multiply(1,2))
+print(divide(1,2))
+
+def create_name(first, last):
+    first = first.capitalize()
+    last = last.capitalize()
+    return first + " " + last
+
+full_name = create_name("annie", "vo")
+print(full_name)
